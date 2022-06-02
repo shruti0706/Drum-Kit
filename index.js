c06c42af921a4487fd5051a2ba726ecd8a441d0e
Shruti Agrawal <72216138+shruti0706@users.noreply.github.com>
2022-06-02 17:34:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Detecting mouse click press
 var numberofdrums=document.querySelectorAll(".drum").length;
-
+ 
 for(var i=0;i<numberofdrums;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 
