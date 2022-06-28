076acdd2e66fe529a4824cacf5a0e37b40288867
Shruti Agrawal <72216138+shruti0706@users.noreply.github.com>
2022-06-28 07:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var numberofdrums=document.querySelectorAll(".drum").length;
  
 for(var i=0;i<numberofdrums;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-
+ 
     var buttonInnerHTML=this.innerHTML;
     MakeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
